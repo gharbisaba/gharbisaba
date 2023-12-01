b6c8831708438e8f0315d52279a0cf93cb122d6a
gharbisaba <152620728+gharbisaba@users.noreply.github.com>
2023-12-01 17:14:49 +0330

    Create README.py

---
+++ README.py
@@ -0,0 +1,12 @@
+op = input( " enter symbol (+,-,*,/) : ")
+x = input( " enter first number: ")
+y = input( " enter second number: ")
+if op == "+":
+  print(x+y)
+if op == "-":
+  print(x-y)
+if op == "*":
+  print(x*y)
+if op == "/":
+  print(x/y)
+
