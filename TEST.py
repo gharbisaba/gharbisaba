df3194cb68f39cb3e7881648a3b2f6c3e2cfc3ac
gharbisaba <152620728+gharbisaba@users.noreply.github.com>
2023-12-01 18:03:25 +0330

    Create TEST.py

---
+++ TEST.py
@@ -0,0 +1,7 @@
+a = input(" enter first number: ")
+b = input(" enter second number: ")
+c = input(" enter third number: ")
+if (a+b>c) and (a+c>b) and (b+c>a):
+  print ( "yes")
+else:
+  print(" no ")
